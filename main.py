ee5f9eb10227dc4d6eca54290b21f4c157c22588
Arthur Chionh (Hwai Teck) <arthur95chionh@gmail.com>
2021-03-16 22:25:37 +0800

    append data to dictionaries, create dataframe

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
 import datetime
 import requests
+import pandas as pd
 
-TOKEN = "BQAPI3LgoqO2RC9KLbPnlelmVRf_7AxIruAH0-jPeRl72GTSqRiGwUZw5VDMc1adQx_IL-xn_KREHEt60mdU36umfL3mkwt0JCmnayEeEzOcf8o2F77E8MZPDB9bZES0hMDkQOxgY4lw6anA5fnZ6qComvbv91VoqDWSWHzjI_K3oDkZNeroLQ"
+TOKEN = "BQABhg8g6s_ZUgZfDOWHedzRW2UoqN3Om8GDvRLoYf1DO-rC74zZKovjIRJ7drqDgTIs6njNOl4Eh20Uet_l9L_6ofGCpVYUQd9rrxcDSOuelrr3t44QIBQlJezLaxyRdB1teNgKrOOCq9xunkfR9oWy4Lh8cqtv2MDPwsUU07SNoYpUj35-_Q"
 
 if __name__ == "__main__":
 
@@ -16,6 +17,26 @@ if __name__ == "__main__":
     yesterday = today - datetime.timedelta(days=2)
     yesterday_unix = int(yesterday.timestamp()) * 1000 #convert unix in seconds to milliseconds for spotify
 
-    r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix), headers=headers)
+    r = requests.get("https://api.spotify.com/v1/me/player/recently-played?&after={time}".format(time=yesterday_unix), headers=headers)
     data = r.json()
-    print(data)
+    
+    song_names = []
+    artist_names = []
+    played_at = []
+    timestamps = []
+
+    for song in data["items"]:
+        song_names.append(song["track"]["name"])
+        artist_names.append(song["track"]["album"]["artists"][0]["name"])
+        played_at.append(song["played_at"])
+        timestamps.append(song["played_at"][0-10]) #only date, no time
+
+    song_dict = {
+        "song_name": song_names,
+        "artist_name": artist_names,
+        "played_at": played_at,
+        "timestamp": timestamps
+    }
+
+    song_df = pd.DataFrame(song_dict, columns=["song_name", "artist_name", "played_at", "timestamp"])
+    print(song_df)
